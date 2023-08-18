0480f429861b294912226c94814d0cd0ddfc3fed
Krushna Jadhav <jadhavkrushna449@gmail.com>
2023-08-18 23:42:31 +0530

    this file contains all the icons which are used in this application

--- src/clients/icons/index.js
+++ src/clients/icons/index.js
@@ -1,21 +1,75 @@
-import CancelIcon from "./CancelIcon";
-import DoneIcon from "./DoneIcon";
-import FilterIcon from "./FilterIcon";
-import HeadDownIcon from "./HeadDownIcon";
-import HighPriority from "./HighPriority";
-import LowPriority from "./LowPriority";
-import MediumPriority from "./MediumPriority";
-import NoPriorityIcon from "./NoPriorityIcon";
-import ProgressIcon from "./ProgressIcon";
+// Status releted icon
 
-export {
-  FilterIcon,
-  HeadDownIcon,
-  HighPriority,
-  LowPriority,
-  MediumPriority,
-  NoPriorityIcon,
-  ProgressIcon,
-  CancelIcon,
-  DoneIcon,
-};
+export const StatusDone = () => (
+  <span style={{ color: "#2563eb" }}>
+    <i class="bi bi-check-circle-fill"></i>
+  </span>
+);
+
+export const StatusProgress = () => (
+  <span style={{ color: "#fbbf24" }}>
+    <i class="bi bi-circle-half"></i>
+  </span>
+);
+
+export const StatusBacklog = () => (
+  <span style={{ color: "#dfe0e5" }}>
+    <i class="bi bi-dash-circle-dotted"></i>
+  </span>
+);
+
+export const StatusCancel = () => (
+  <span style={{ color: "#dfe0e5" }}>
+    <i class="bi bi-x-circle-fill"></i>
+  </span>
+);
+
+export const StatusTodo = () => (
+  <span style={{ color: "#dfe0e5" }}>
+    <i class="bi bi-circle"></i>
+  </span>
+);
+
+// priority releted icons
+export const PriorityNo = ({color}) => (
+  <span style={{ color: color || "#dfe0e5" }}>
+    <i class="bi bi-three-dots"></i>
+  </span>
+);
+
+export const PriorityUrgent = () => (
+  <span style={{ color: "#fb923c"}}>
+    <i class="bi bi-exclamation-square-fill"></i>
+  </span>
+);
+
+export const PriorityLow = ({color}) => (
+  <span style={{ color: color || "#dfe0e5" }}>
+    <i class="bi bi-wifi-1"></i>
+  </span>
+);
+
+export const PriorityMedium = ({color}) => (
+  <span style={{ color: color || "#dfe0e5" }}>
+    <i class="bi bi-wifi-2"></i>
+  </span>
+);
+
+export const PriorityHigh = ({color}) => (
+  <span style={{ color: color || "#dfe0e5" }}>
+    <i class="bi bi-wifi"></i>
+  </span>
+);
+
+export const PlusIcon = ({color}) => (
+  <span style={{ color: color || "#dfe0e5" }}>
+    <i class="bi bi-plus-lg"></i>
+  </span>
+);
+
+export const HeadDown = ({color}) => (
+  <span style={{ color: color || "#dfe0e5" }}>
+    <i class="bi bi-chevron-down"></i>
+  </span>
+);
+
