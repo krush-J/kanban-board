8fe08675df96ddcb28c321f06343d41a22899238
Krushna Jadhav <jadhavkrushna449@gmail.com>
2023-08-18 23:57:57 +0530

    export modules

---
+++ src/clients/components/index.js
@@ -0,0 +1,11 @@
+import SelectCustom from "./SelectCustom";
+import Card from './Card'
+import Avtar from "./Avtar";
+import GroupHeader from "./GroupHeader";
+export{
+    SelectCustom,
+    Card,
+    Avtar,
+    GroupHeader
+
+}
